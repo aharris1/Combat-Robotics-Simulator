93390d66b97aac50c5e695e571394d12263ad592
aharris <aharris1@stevens.edu>
2018-01-14 02:48:15 -0500

    Initial commit
    Initial commit of files

---
+++ Combat Simulator/Assets/MotorScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorScript : MonoBehaviour {
+
+    WheelCollider wheel;
+
+	// Use this for initialization
+	void Start () {
+        print("TEST");
+        GameObject wheelobj = GameObject.FindGameObjectWithTag("wheel");
+        wheel = wheelobj.GetComponent<WheelCollider>();
+        print(wheel.isGrounded);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(wheel.isGrounded);
+        if (Input.GetKey("up"))
+        {
+            wheel.brakeTorque = 0;
+            wheel.motorTorque = 30 - Math.Abs(wheel.rpm);
+            print(wheel.motorTorque);
+        }
+        else if(Input.GetKey("down"))
+        {
+            wheel.brakeTorque = 0;
+            wheel.motorTorque = -(30 - Math.Abs(wheel.rpm));
+            print(wheel.motorTorque);
+        }
+        else
+        {
+            wheel.motorTorque = 0;
+            wheel.brakeTorque = 1;
+        }
+	}
+}
---
+++ Combat Simulator/Assets/SwerveDriveController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwerveDriveController : MonoBehaviour {
+    public WheelCollider frontLeft;
+    public WheelCollider frontRight;
+    public WheelCollider backLeft;
+    public WheelCollider backRight;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up"))
+        {
+            setAllMotorTorques(10);    
+        }
+        else if (Input.GetKey("down"))
+        {
+            setAllMotorTorques(10);
+        }
+        else
+        {
+            setAllMotorTorques(0);
+        }
+        if (Input.GetKey("left"))
+        {
+            incrementAllSteerAngles(-2);
+        }
+        else if (Input.GetKey("right"))
+        {
+            incrementAllSteerAngles(2);
+        }
+
+	}
+
+    void incrementAllSteerAngles(float degrees)
+    {
+        frontLeft.steerAngle = (frontLeft.steerAngle + degrees) % 360;
+        frontRight.steerAngle = (frontRight.steerAngle + degrees) % 360;
+        backLeft.steerAngle = (backLeft.steerAngle + degrees) % 360;
+        backRight.steerAngle = (backRight.steerAngle + degrees) % 360;
+    }
+
+    void setAllSteerAngles(float degrees)
+    {
+        frontLeft.steerAngle = degrees;
+        frontRight.steerAngle = degrees;
+        backLeft.steerAngle = degrees;
+        backRight.steerAngle = degrees;
+    }
+
+    void setAllBrakeTorque(float torque)
+    {
+        frontLeft.brakeTorque = torque;
+        frontRight.brakeTorque = torque;
+        backLeft.brakeTorque = torque;
+        backRight.brakeTorque = torque;
+    }
+
+    void setAllMotorTorques(float torque)
+    {
+        frontLeft.motorTorque = torque;
+        frontRight.motorTorque = torque;
+        backLeft.motorTorque = torque;
+        backRight.motorTorque = torque;
+    }
+}
